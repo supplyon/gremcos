5f9d197b2c79a0ef904c3c16b33a658f30fe0ff4
Thomas Obenaus <obenaus.thomas@gmail.com>
2022-05-11 11:32:43 +0200

    WIP

--- dialerFactory.go
+++ dialerFactory.go
@@ -1,20 +1,23 @@
 package gremcos
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"time"
 
 	gorilla "github.com/gorilla/websocket"
+	"github.com/rs/zerolog"
 	"github.com/supplyon/gremcos/interfaces"
 )
 
-// websocketDialer is a function type for dialing/ connecting to a websocket server and creating a WebsocketConnection
+// WebsocketDialer is a function type for dialing/ connecting to a websocket server and creating a WebsocketConnection
 type websocketDialer func(urlStr string, requestHeader http.Header) (interfaces.WebsocketConnection, *http.Response, error)
 
-// websocketDialerFactory is a function type that is able to create websocketDialer's
+// websocketDialerFactory is a function type that is able to create WebsocketDialer's
 type websocketDialerFactory func(writeBufferSize, readBufferSize int, handshakeTimout time.Duration) websocketDialer
 
-// gorillaWebsocketDialerFactory is a function that is able to create websocketDialer's using the websocket implementation
+// gorillaWebsocketDialerFactory is a function that is able to create WebsocketDialer's using the websocket implementation
 // of github.com/gorilla/websocket
 var gorillaWebsocketDialerFactory = func(writeBufferSize, readBufferSize int, handshakeTimout time.Duration) websocketDialer {
 	// create the gorilla websocket dialer
@@ -24,8 +27,37 @@ var gorillaWebsocketDialerFactory = func(writeBufferSize, readBufferSize int, ha
 		HandshakeTimeout: handshakeTimout,
 	}
 
-	// return the websocketDialer, wrapping the gorilla websocket dial call
+	// return the WebsocketDialer, wrapping the gorilla websocket dial call
 	return func(urlStr string, requestHeader http.Header) (interfaces.WebsocketConnection, *http.Response, error) {
 		return dialer.Dial(urlStr, requestHeader)
+	}
+
+}
+
+var DebugGorillaWebsocketDialerFactory = func(logger zerolog.Logger) func(writeBufferSize, readBufferSize int, handshakeTimout time.Duration) websocketDialer {
+	return func(writeBufferSize, readBufferSize int, handshakeTimout time.Duration) websocketDialer {
+		netDialer := &net.Dialer{}
+		netDial := func(network, addr string) (net.Conn, error) {
+			dialStart := time.Now()
+			conn, err := netDialer.DialContext(context.Background(), network, addr)
+			if err != nil {
+				dialDur := time.Since(dialStart)
+				logger.Warn().Err(err).Msgf("Dialing %#v %#v failed: %v after %s", network, addr, err, dialDur)
+			}
+			return conn, err
+		}
+
+		// create the gorilla websocket dialer
+		dialer := gorilla.Dialer{
+			WriteBufferSize:  writeBufferSize,
+			ReadBufferSize:   readBufferSize,
+			HandshakeTimeout: handshakeTimout,
+			NetDial:          netDial,
+		}
+
+		// return the WebsocketDialer, wrapping the gorilla websocket dial call
+		return func(urlStr string, requestHeader http.Header) (interfaces.WebsocketConnection, *http.Response, error) {
+			return dialer.Dial(urlStr, requestHeader)
+		}
 	}
 }
